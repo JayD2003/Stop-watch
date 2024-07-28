050ee24b81fdc2c1b050aa5a807a71391e04173f
JayD2003 <jayasaidodla@gmail.com>
2024-07-29 00:35:22 +0530

    completed stop watch

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Stopwatch from './Stopwatch'
+
+function App() {
+
+  return (
+    <div class="main">
+      <h1>Stop Watch</h1>
+      <Stopwatch />
+    </div>
+  )
+}
+
+export default App
